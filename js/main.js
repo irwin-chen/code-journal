d325f0a4d154c780f82b06bddfcbc143da1f6a87
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-05 18:39:23 +0000

    completed submit events on journal entry form

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 /* global data */
 /* exported data */
 
-var $photoURL = document.querySelector('#photo-url');
+var $photo = document.querySelector('#photo');
 var $image = document.querySelector('img');
-$photoURL.addEventListener('input', function () {
-  $image.src = $photoURL.value;
-  if ($photoURL.value === '') {
+var $submit = document.querySelector('.new-entry-button');
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
+var $form = document.querySelector('form');
+
+$photo.addEventListener('input', function () {
+  $image.src = $photo.value;
+  if ($photo.value === '') {
     $image.src = 'images/placeholder-image-square.jpg';
   }
+});
+
+$submit.addEventListener('click', function (event) {
+  event.preventDefault();
+  var dataEntry = {
+    title: $title.value,
+    photo: $photo.value,
+    notes: $notes.value,
+    nextEntryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push(dataEntry);
+  $image.src = 'images/placeholder-image-square.jpg';
+  $form.reset();
 });
