a4d8cf4cfb761e030f70c5be654a13a0f36bf038
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-05 01:55:38 +0000

    in prog

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $photoURL = document.querySelector('#photo-url');
+$photoURL.addEventListener('input', function (event) {
+  // var $image = document.querySelector('img');
+
+  // $image.src =
+});
